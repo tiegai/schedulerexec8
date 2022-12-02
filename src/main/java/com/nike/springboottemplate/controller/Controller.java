8caece2762fa84e1b53a0912e40daca5a8686350
jch527_nike <Jerry.Chen3@nike.com>
2022-12-02 11:11:02 +0800

    11

--- src/main/java/com/nike/springboottemplate/controller/Controller.java
+++ src/main/java/com/nike/springboottemplate/controller/Controller.java
@@ -1,4 +1,4 @@
-package com.nike.springboottemplate;
+package com.nike.springboottemplate.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
--- src/test/java/com/nike/springboottemplate/ControllerTest.java
+++ src/test/java/com/nike/springboottemplate/ControllerTest.java
@@ -1,5 +1,6 @@
 package com.nike.springboottemplate;
 
+import com.nike.springboottemplate.controller.Controller;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
