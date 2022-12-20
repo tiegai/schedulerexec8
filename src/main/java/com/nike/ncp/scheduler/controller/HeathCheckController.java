7ad2f6ed86f20eb8695804a6aa1b5ba76440d646
jch527_nike <Jerry.Chen3@nike.com>
2022-12-20 15:23:36 +0800

    add healthcheck

--- src/main/java/com/nike/ncp/scheduler/controller/HeathCheckController.java
+++ src/main/java/com/nike/ncp/scheduler/controller/HeathCheckController.java
@@ -2,14 +2,13 @@ package com.nike.ncp.scheduler.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/springboot_demo")
-public class Controller {
+public class HeathCheckController {
+
     @GetMapping("/healthcheck")
-    public ResponseEntity<Object> healthcheck() {
+    public ResponseEntity<Void> healthCheck() {
         return ResponseEntity.ok().build();
     }
 }
